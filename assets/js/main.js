ec77e5df21f6b637d14f567df00d15862d358e70
ASUS-X555UJ\Admin <sangtranwp@gmail.com>
2022-09-13 14:02:08 +0700

    Upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function changeImg(img){
+    document.querySelector('.main-img').src = img;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
