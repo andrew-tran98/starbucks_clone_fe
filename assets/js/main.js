e00ca8d858705f611c1d4e26bc5e2cb7556fd114
ASUS-X555UJ\Admin <sangtranwp@gmail.com>
2022-09-26 22:40:16 +0700

    Update Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+
+function myFunction(){
+    var x = document.querySelector('#list');
+    if (x.style.display == "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block"
+    }
+}
